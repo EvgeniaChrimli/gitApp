cc783078f2d58d72f6626bb5c8d80ccc16f2985d
Хримли Евгения <evichkanova@list.ru>
2025-05-31 14:05:32 +0300

    vercel

--- server/api/index.js
+++ server/api/index.js
@@ -50,6 +50,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
